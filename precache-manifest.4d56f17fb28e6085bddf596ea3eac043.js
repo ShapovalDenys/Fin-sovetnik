3e62e3a187a26fbf071d7202f320c9eafd73db9d
ShapovalDenys <denys.igorevich@gmail.com>
2020-08-13 18:24:30 +0300

    Updates

---
+++ precache-manifest.4d56f17fb28e6085bddf596ea3eac043.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ab6b705f8d21d204b5aa2fb533b4a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d9e60355f31cdc17fd2",
+    "url": "./static/css/main.482c3dff.chunk.css"
+  },
+  {
+    "revision": "d2fbcc3e2237e0b55ffa",
+    "url": "./static/js/2.3bb2ef63.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3bb2ef63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9e60355f31cdc17fd2",
+    "url": "./static/js/main.9fd0ecd7.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "98bdc6422d7af820b3a2e02a9c77cc39",
+    "url": "./static/media/Footer.98bdc642.png"
+  },
+  {
+    "revision": "d0155a85638abdf370b0e8e5ac63128c",
+    "url": "./static/media/head.d0155a85.png"
+  },
+  {
+    "revision": "6581cfaeee8057734a3f16d37719bfb2",
+    "url": "./static/media/segoeui.6581cfae.ttf"
+  }
+]);
